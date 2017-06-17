4ad632105257ba8c485e17029dad1404bc16adb8
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-06-17 23:04:16 +0200

    added parameters file

--- clustering.py
+++ clustering.py
@@ -2,9 +2,10 @@ import pandas
 from numpy import array
 from scipy.cluster.vq import kmeans, whiten
 
+import parameters
 from functions import map_runs, get_closest_run_to_mean, map_dict_to_list
 
-df = pandas.read_csv('resources/out.csv', sep=';', usecols=['runId', 'instanceId', 'score'])
+df = pandas.read_csv('resources/out.csv', sep=';', usecols=parameters.cols)
 runs = map_runs(df)
 
 sorted_keys = sorted(runs.keys())
@@ -12,8 +13,7 @@ dictListed = map_dict_to_list(runs)
 
 features = array(dictListed)
 whitened = whiten(features)
-k = 10
-means = kmeans(whitened, k)
+means = kmeans(whitened, parameters.k)
 
 for mean in means[0]:
     print get_closest_run_to_mean(mean, whitened, sorted_keys)
