4e6d8b22cccacba1d6995ae9abb35f58baf421c6
Krzysztof Wędrowicz <krzysztof.wedrowicz@espeo.eu>
2017-09-07 07:52:48 +0200

    added mapping script for optilion out.csv

---
+++ run_mapping.py
@@ -0,0 +1,10 @@
+import pandas
+
+import parameters
+
+df = pandas.read_csv('resources/out.csv', sep=';', usecols=parameters.cols, dtype='int')
+runs = df.pivot(index='runId', columns='instanceId', values='score')
+runs = runs.fillna(0).astype(int)
+
+runs.to_csv('resources/lumberjack.csv')
+
